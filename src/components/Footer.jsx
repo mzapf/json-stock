14b6af729d69bedd37a99fb2ec01690bde94a22d
Matias Zapf <mzapf@pm.me>
2023-11-07 01:14:27 -0300

    Refactor Footer and add stock differences feature

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,16 +3,16 @@ import { FaGithub } from 'react-icons/fa';
 
 const Footer = () => {
     return (
-        <footer className="flex justify-center items-center p-4 bg-gray-100 border-t mt-8">
+        <footer className="p-4 bg-gray-100 border-t mt-8">
             <a
                 href="https://github.com/mzapf/json-stock"
-                className="text-gray-700 hover:text-black"
+                className="text-gray-700 hover:text-black justify-center flex items-center font-bold"
                 target="_blank"
                 rel="noopener noreferrer"
                 title="Ver el código fuente en GitHub"
             >
                 <FaGithub className="inline-block text-xl mr-2" />
-                Ver en GitHub
+                Matias Zapf
             </a>
         </footer>
     );
--- src/components/StockContainer.jsx
+++ src/components/StockContainer.jsx
@@ -7,6 +7,11 @@ const StockContainer = ({ data }) => {
     const [adjustedStock, setAdjustedStock] = useState({});
     const [selectedCategory, setSelectedCategory] = useState(null);
     const [selectedSubcategory, setSelectedSubcategory] = useState(null);
+    const [showDifferences, setShowDifferences] = useState(false);
+
+    const toggleDifferences = () => {
+        setShowDifferences(!showDifferences);
+    };
 
     useEffect(() => {
         initializeAdjustedStock(data);
@@ -47,45 +52,105 @@ const StockContainer = ({ data }) => {
         });
     };
 
+    const findDifferences = (adjustedStock, originalStock) => {
+        let differences = {};
+
+        for (const category of Object.keys(adjustedStock)) {
+            for (const subcategory of Object.keys(adjustedStock[category])) {
+                for (const item of Object.keys(adjustedStock[category][subcategory])) {
+                    let adjustedValue = adjustedStock[category][subcategory][item];
+                    let originalValue = originalStock[category][subcategory][item];
+
+                    if (adjustedValue !== originalValue) {
+                        if (!differences[category]) differences[category] = {};
+                        if (!differences[category][subcategory]) differences[category][subcategory] = [];
+
+                        differences[category][subcategory].push({
+                            item,
+                            adjustedValue,
+                            originalValue,
+                        });
+                    }
+                }
+            }
+        }
+
+        return differences;
+    };
+
+
+    const differences = findDifferences(adjustedStock, data);
+
     return (
         <div>
-            {Object.keys(stock).map((category) => (
-                <div key={category}>
-                    <CategoryButton
-                        category={category}
-                        isSelected={selectedCategory === category}
-                        onClick={() => setSelectedCategory(category)}
-                    />
-                    {selectedCategory === category && (
-                        <div>
-                            {Object.keys(stock[category]).map((subcategory) => (
-                                <div key={subcategory}>
-                                    <CategoryButton
-                                        category={subcategory}
-                                        isSelected={selectedSubcategory === subcategory}
-                                        onClick={() => setSelectedSubcategory(subcategory)}
-                                        isSubcategory={true}
-                                    />
-                                    {selectedSubcategory === subcategory && (
-                                        <div>
-                                            {Object.entries(stock[category][subcategory]).map(([itemName, value]) => (
-                                                <ItemControls
-                                                    key={itemName}
-                                                    itemName={itemName}
-                                                    originalValue={value}
-                                                    adjustedValue={adjustedStock[category]?.[subcategory]?.[itemName] || 0}
-                                                    onIncrement={(amount) => handleIncrement(category, subcategory, itemName, amount)}
-                                                    onDecrement={(amount) => handleDecrement(category, subcategory, itemName, amount)}
-                                                />
-                                            ))}
-                                        </div>
-                                    )}
-                                </div>
-                            ))}
-                        </div>
+            <div>
+                {Object.keys(stock).map((category) => (
+                    <div key={category}>
+                        <CategoryButton
+                            category={category}
+                            isSelected={selectedCategory === category}
+                            onClick={() => setSelectedCategory(category)}
+                        />
+                        {selectedCategory === category && (
+                            <div>
+                                {Object.keys(stock[category]).map((subcategory) => (
+                                    <div key={subcategory}>
+                                        <CategoryButton
+                                            category={subcategory}
+                                            isSelected={selectedSubcategory === subcategory}
+                                            onClick={() => setSelectedSubcategory(subcategory)}
+                                            isSubcategory={true}
+                                        />
+                                        {selectedSubcategory === subcategory && (
+                                            <div>
+                                                {Object.entries(stock[category][subcategory]).map(([itemName, value]) => (
+                                                    <ItemControls
+                                                        key={itemName}
+                                                        itemName={itemName}
+                                                        originalValue={value}
+                                                        adjustedValue={adjustedStock[category]?.[subcategory]?.[itemName] || 0}
+                                                        onIncrement={(amount) => handleIncrement(category, subcategory, itemName, amount)}
+                                                        onDecrement={(amount) => handleDecrement(category, subcategory, itemName, amount)}
+                                                    />
+                                                ))}
+                                            </div>
+                                        )}
+                                    </div>
+                                ))}
+                            </div>
+                        )}
+                    </div>
+                ))}
+            </div>
+            <button
+                className="bg-blue-500 text-white py-2 px-4 rounded mt-4"
+                onClick={toggleDifferences}
+            >
+                {showDifferences ? 'Ocultar Diferencias de Stock' : 'Ver Diferencias de Stock'}
+            </button>
+            {showDifferences && (
+                <div className="my-2">
+                    {Object.keys(differences).length > 0 ? (
+                        Object.entries(differences).map(([category, subcategories]) => (
+                            <div key={category}>
+                                <h1 className="text-xl font-semibold">{category}</h1>
+                                {Object.entries(subcategories).map(([subcategory, items]) => (
+                                    <div key={subcategory}>
+                                        <h2 className="text-lg font-semibold ml-4">{subcategory}</h2>
+                                        {items.map((item) => (
+                                            <div key={item.item} className="ml-8">
+                                                {`${item.item}: debería haber ${item.originalValue} y hay ${item.adjustedValue}`}
+                                            </div>
+                                        ))}
+                                    </div>
+                                ))}
+                            </div>
+                        ))
+                    ) : (
+                        <p>No hay diferencias.</p>
                     )}
                 </div>
-            ))}
+            )}
         </div>
     );
 };
